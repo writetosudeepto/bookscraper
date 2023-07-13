b8d256d2f488e3e8002f9d5b4cd8d6caee6c6a30
writetosudeepto <writetosudeepto@gmail.com>
2023-07-13 14:03:34 +0530

    pipelines created

--- bookscraper/items.py
+++ bookscraper/items.py
@@ -10,3 +10,23 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+def serialize_price(value):
+    return f'$ {str(value)}'
+
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()

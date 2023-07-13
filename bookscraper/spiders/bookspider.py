068d204c4f85ae13918ad44c9baf53c9b5216d7d
writetosudeepto <writetosudeepto@gmail.com>
2023-07-13 14:03:05 +0530

    pipelines created

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from bookscraper.items import BookItem
 
 
 class BookspiderSpider(scrapy.Spider):
@@ -36,18 +37,21 @@ class BookspiderSpider(scrapy.Spider):
         book = response.css("div.product_main")[0]
         table_rows = response.css("table tr")
 
-        yield {
-            'url': response.url,
-            'title': book.css("h1 ::text").get(),
-            'upc': table_rows[0].css("td ::text").get(),
-            'product_type': table_rows[1].css("td ::text").get(),
-            'price_excl_tax': table_rows[2].css("td ::text").get(),
-            'price_incl_tax': table_rows[3].css("td ::text").get(),
-            'tax': table_rows[4].css("td ::text").get(),
-            'availability': table_rows[5].css("td ::text").get(),
-            'num_reviews': table_rows[6].css("td ::text").get(),
-            'stars': book.css("p.star-rating").attrib['class'],
-            'category': response.xpath("//ul[@class='breadcrumb']/li[3]/a/text()").get(),
-            'product_description': book.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-            'price': book.css('p.price_color ::text').get(),
-        }
+        book_item = BookItem()
+        book_item['url'] = response.url
+        book_item['title'] = book.css("h1 ::text").get()
+        book_item['upc'] = table_rows[0].css("td ::text").get()
+        book_item['product_type'] = table_rows[1].css("td ::text").get()
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get()
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get()
+        book_item['tax'] = table_rows[4].css("td ::text").get()
+        book_item['availability'] = table_rows[5].css("td ::text").get()
+        book_item['num_reviews'] = table_rows[6].css("td ::text").get()
+        book_item['stars'] = book.css("p.star-rating").attrib['class']
+        book_item['category'] = response.xpath(
+            "//ul[@class='breadcrumb']/li[3]/a/text()").get()
+        book_item['product_description'] = book.xpath(
+            "//div[@id='product_description']/following-sibling::p/text()").get()
+        book_item['price'] = book.css('p.price_color ::text').get()
+
+        yield book_item
